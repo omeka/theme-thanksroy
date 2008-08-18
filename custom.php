c89f52e9aaea9b74dfe84830581414e51126300c
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-08-18 20:43:25 +0000

    Adds the custom.php script to Omeka's default theme.

---
+++ custom.php
@@ -0,0 +1,5 @@
+<?php 
+// Use this file to define customized helper functions, filters or 'hacks' defined
+// specifically for use in your Omeka theme. Note that helper functions that are
+// designed for portability across themes should be grouped into a plugin whenever
+// possible.
