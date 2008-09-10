97fe3885862fc5cc01c840e84581cac260f3e03c
Ken Albers <ken.albers@gmail.com>
2008-09-10 19:25:09 +0000

    Upgraded the collection/show page to use the new api.

--- collections/show.php
+++ collections/show.php
@@ -1,24 +1,23 @@
-<?php head(array('title'=>h($collection->name))); ?>
+<?php head(array('title'=>collection('Title'))); ?>
 
 <div id="primary" class="show">
-    <h1><?php echo h($collection->name); ?></h1>
+    <h1><?php echo collection('Title'); ?></h1>
 
-    <div id="description" class="field">
+    <div id="description" class="element">
     <h2>Description</h2>
-    <div class="field-value"><?php echo nls2p(h($collection->description)); ?></div>
+    <div class="element-text"><?php echo nls2p(collection('Description')); ?></div>
     </div>
     
-    <div id="collectors" class="field">
+    <div id="collectors" class="element">
     <h2>Collector(s)</h2> 
-        <div class="field-value">
-            <ul><?php foreach($collection->Collectors as $collector):?>
-                <li><?php echo h($collector->name); ?></li>
-                <?php endforeach; ?>
+        <div class="element-text">
+            <ul>
+                <li><?php echo collection('Collectors', array('delimiter'=>'</li><li>')); ?></li>
             </ul>
         </div>
     </div>
 
-    <p><a href="<?php echo uri('items/browse/', array('collection'=>$collection->id)); ?>">View the items in &quot;<?php echo h($collection->name); ?>&quot;</a></p>
+    <p class="view-items-link"><?php echo link_to_browse_items('View the items in' . collection('Title'), array('collection' => collection('id'))); ?></p>
 
 </div>
 
--- items/browse.php
+++ items/browse.php
@@ -11,38 +11,38 @@ endif;
 		<ul class="navigation" id="secondary-nav">
 			<?php echo nav(array('Browse All' => uri('items'), 'Browse by Tag' => uri('items/tags'))); ?>
 		</ul>
-		<?php echo htmlentities($_GET['tag']);?>
+		
 		<div class="pagination top"><?php echo pagination_links(); ?></div>
-		<?php foreach($items as $key => $item): ?>
+		<?php while (loop_items()): ?>
 			<div class="item hentry">
 				<div class="item-meta">
-				<h3><?php echo link_to_item($item, 'show', null, array('class'=>'permalink')); ?></h3>
+				<h3><?php echo link_to_item(item('Title'), array('class'=>'permalink')); ?></h3>
 
-				<?php if(has_thumbnail($item)): ?>
+				<?php if (item_has_thumbnail()): ?>
 				<div class="item-img">
-					<?php echo link_to_square_thumbnail($item); ?>						
+					<?php echo link_to_item(item_square_thumbnail(array('alt'=>item('Title',0)))); ?>						
 				</div>
 				<?php endif; ?>
 
-				<?php if($text = item_metadata($item,'Text')): ?>
+				<?php if ($text = item('Text', array('index'=>0,'snippet'=>250))): ?>
 	    			<div class="item-description">
-    				<p><?php echo snippet($text,0,250); ?></p>
+    				<p><?php echo $text; ?></p>
     				</div>
-				<?php elseif(!empty($item->description)): ?>
+				<?php elseif ($description = item('Description', array('index'=>0,'snippet'=>250))): ?>
     				<div class="item-description">
-    				<?php echo nls2p(h(snippet($item->description, 0, 250))); ?>
+    				<?php echo $description; ?>
     				</div>
 				<?php endif; ?>
 
-				<?php if(count($item->Tags)): ?>
+				<?php if (item_has_tags()): ?>
 				<div class="tags"><p><strong>Tags:</strong>
-				<?php echo tag_string($item, uri('items/browse/tag/')); ?></p>
+				<?php echo item_tags_as_string(); ?></p>
 				</div>
 				<?php endif;?>
 
 				</div>
 			</div>
-		<?php endforeach; ?>
+		<?php endwhile; ?>
 		<div class="pagination bottom"><?php echo pagination_links(); ?></div>
 			
 	</div>
