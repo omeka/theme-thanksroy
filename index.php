8285035c09cc233164a0cb0224f66d2095b8a069
Jim Safley <jimsafley@gmail.com>
2012-09-20 16:11:46 -0400

    Update helper functions.

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     <?php if (get_theme_option('Display Featured Collection')): ?>
     <!-- Featured Collection -->
     <div id="featured-collection">
-        <?php echo display_random_featured_collection(); ?>
+        <?php echo random_featured_collection(); ?>
     </div><!-- end featured collection -->
     <?php endif; ?>	
     <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
