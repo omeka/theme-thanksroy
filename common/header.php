9265c72d302f1691b24cb26ee8fa17849deba4ba
Kim Nguyen <design@kimisgold.com>
2012-10-05 18:50:01 -0400

    Add font family, remove whitespace, move wrap container.

--- common/header.php
+++ common/header.php
@@ -14,6 +14,7 @@
     <?php fire_plugin_hook('public_theme_header'); ?>
 
     <!-- Stylesheets -->
+    <link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <?php
     queue_css_file('style');
     echo head_css();
@@ -24,23 +25,21 @@
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
     <?php fire_plugin_hook('public_theme_body'); ?>
-    	<div id="wrap">
-    
-    		<div id="header">
-    		<?php fire_plugin_hook('public_theme_page_header'); ?>
-    		<div id="site-title"><?php echo link_to_home_page(custom_display_logo()); ?></div>
-    		</div>
-    		
-    		<div id="content">
-    		    <?php fire_plugin_hook('public_theme_page_content'); ?>
-    			<div id="primary-nav">
-    				<div id="search-wrap">
-    				    <h2>Search</h2>
-    				    <?php echo simple_search_form(); ?>
-    				    <?php echo link_to_item_search(); ?>
-        			</div>
-        			
-        			<ul class="navigation">
-        			    <?php echo custom_public_nav_header(); ?>
-        			</ul>
-    			</div>
+
+            <header>
+                <?php fire_plugin_hook('public_theme_page_header'); ?>
+                <div id="site-title"><?php echo link_to_home_page(custom_display_logo()); ?></div>
+            </header>
+
+
+        <div id="wrap">                
+            <div id="content">
+                <?php fire_plugin_hook('public_theme_page_content'); ?>
+                <nav id="primary-nav">
+                    <div id="search-wrap">
+                        <h2>Search</h2>
+                        <?php echo simple_search_form(); ?>
+                        <?php echo link_to_item_search(); ?>
+                    </div>
+                    <?php echo public_nav_main(); ?>
+                </nav>
