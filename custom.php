a92910969541ab6cfc4ae64e6ec7c61574b1dbb9
Kim Nguyen <design@kimisgold.com>
2012-10-08 18:27:32 -0400

    Add custom header function to display thumbnail instead of square thumbail.

---
+++ custom.php
@@ -0,0 +1,22 @@
+<?php 
+
+function thanksroy_random_featured_item() 
+{
+    $html = '<h2>'. __('Featured Item') .'</h2>';
+    if ($randomFeaturedItems = get_random_featured_items(1)) {
+        $randomItem = $randomFeaturedItems[0];
+        $itemTitle = metadata($randomItem, array('Dublin Core', 'Title'));
+        if (item_has_thumbnail($randomItem)) {
+            $html .= link_to_item(item_thumbnail(array(), 0, $randomItem), array('class'=>'image'), 'show', $randomItem);
+        }
+        $html .= '<h3>' . link_to_item($itemTitle, array(), 'show', $randomItem) . '</h3>';
+        if ($itemDescription = metadata($randomItem, array('Dublin Core', 'Description'), array('snippet'=>150))) {
+            $html .= '<p class="item-description">' . $itemDescription . '</p>';
+        }
+    } else {
+        $html .= '<p>'.__('No featured items are available.').'</p>';
+    }
+    return $html;
+}
+
+?>
