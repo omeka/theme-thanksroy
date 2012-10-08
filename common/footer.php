ec5801ab6a60b1dead80377a0a6a8643ff90c076
Kim Nguyen <design@kimisgold.com>
2012-10-08 18:30:54 -0400

    Rearrange footer placement.

--- common/footer.php
+++ common/footer.php
@@ -1,24 +1,21 @@
         </div><!-- end content -->
 
-        <footer>
-
-            <nav>
-                <ul class="navigation">
-                    <?php echo public_nav_main(array(__('Home') => url(''), __('Browse Items') => url('items'), __('Browse Collections') => url('collections'))); ?>
-                </ul>
-            </nav>
-
-            <div id="footer-text">
-                <?php echo get_theme_option('Footer Text'); ?>
-                <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-                    <p><?php echo $copyright; ?></p>
-                <?php endif; ?>
-                <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
-            </div>
-
-            <?php fire_plugin_hook('public_theme_footer'); ?>
-
-        </footer><!-- end footer -->
     </div><!-- end wrap -->
+    
+
+    <footer>
+
+        <div id="footer-text">
+            <?php echo get_theme_option('Footer Text'); ?>
+            <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+                <p><?php echo $copyright; ?></p>
+            <?php endif; ?>
+            <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+        </div>
+
+        <?php fire_plugin_hook('public_theme_footer'); ?>
+
+    </footer><!-- end footer -->
+    
 </body>
 </html>
