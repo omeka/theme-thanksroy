8cddd2e2ec0784f53fae70213cf3eb14582234e1
Kim Nguyen <design@kimisgold.com>
2014-10-09 14:35:47 -0400

    Restructure ExhibitBuilder subnavigation to use vertical layout, accommodating large amounts of organization and content.

--- functions.php
+++ functions.php
@@ -25,3 +25,38 @@ function thanksroy_brighten($color, $steps) {
 
      return '#'.$r_hex.$g_hex.$b_hex;
 }
+
+
+function thanksroy_exhibit_builder_page_summary($exhibitPage = null, $currentPageId)
+{
+    if (!$exhibitPage) {
+        $exhibitPage = get_current_record('exhibit_page');
+    }
+
+    $parents = array();
+    $currentPage = get_record_by_id('Exhibit Page', $currentPageId);
+    while ($currentPage->parent_id) {        
+        $currentPage = $currentPage->getParent();
+        array_unshift($parents, $currentPage->id);
+    }
+    
+    $class  = '';
+    $class .= ($exhibitPage->id == $currentPageId) ? 'current' : '';
+    $parent = (array_search($exhibitPage->id, $parents) !== false) ? ' parent' : '';
+
+    $html = '<li class="' . $class . $parent . '">'
+          . '<a href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) . '">'
+          . metadata($exhibitPage, 'title') .'</a>';
+
+    $children = $exhibitPage->getChildPages();
+    if ($children) {
+        $html .= '<ul>';
+        foreach ($children as $child) {
+            $html .= thanksroy_exhibit_builder_page_summary($child, $currentPageId);
+            release_object($child);
+        }
+        $html .= '</ul>';
+    }
+    $html .= '</li>';
+    return $html;
+}
