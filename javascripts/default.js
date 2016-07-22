f0da0d4bf44012234dae070806a78de9114f3c69
Kim Nguyen <design@kimisgold.com>
2016-07-22 13:57:07 -0400

    Make dropdown glyph consistent, fix dropdown toggle on touch devices, and use double-click to go to navigation parent link on touch devices. (#27)

--- javascripts/default.js
+++ javascripts/default.js
@@ -3,35 +3,35 @@ if (!ThanksRoy) {
 }
 
 (function($,sr){
- 
+
   // debouncing function from John Hann
   // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
   var debounce = function (func, threshold, execAsap) {
       var timeout;
- 
+
       return function debounced () {
           var obj = this, args = arguments;
           function delayed () {
               if (!execAsap)
                   func.apply(obj, args);
-              timeout = null; 
+              timeout = null;
           };
- 
+
           if (timeout)
               clearTimeout(timeout);
           else if (execAsap)
               func.apply(obj, args);
- 
-          timeout = setTimeout(delayed, threshold || 100); 
+
+          timeout = setTimeout(delayed, threshold || 100);
       };
   }
-	// smartresize 
+	// smartresize
 	jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
- 
+
 })(jQuery,'smartresize');
 
 (function ($) {
-    
+
     ThanksRoy.moveNavOnResize = function() {
         var primaryNavUl;
         var moveNav = function () {
@@ -46,23 +46,43 @@ if (!ThanksRoy) {
             }
         }
         moveNav();
-        
+
         $(window).smartresize(function() {
             moveNav();
         });
     };
-    
+
     ThanksRoy.mobileMenu = function() {
         $('.navigation li a').each( function() {
             if ($(this).next().length > 0) {
                 $(this).parent().addClass('parent');
             }
+        })
+
+        // touch device parent navigation link fix from
+        // http://stackoverflow.com/questions/27560653/jquery-on-double-click-event-dblclick-for-mobile
+        var touchtime = 0;
+        $('.navigation').on('touchend', '.parent > a', function() {
+            if(touchtime == 0) {
+                //set first click
+                touchtime = new Date().getTime();
+            } else {
+                //compare first click to this click and see if they occurred within double click threshold
+                if(((new Date().getTime())-touchtime) < 800) {
+                    //double click occurred
+                    touchtime = 0;
+                    window.location.href = $(this).attr('href');
+                } else {
+                    //not a double click so set as a new first click
+                    touchtime = new Date().getTime();
+                }
+            }
         });
-        
+
         $('.menu-button').click( function(e) {
             e.preventDefault();
             $('.mobile').toggle();
         });
     };
-    
+
 })(jQuery);
