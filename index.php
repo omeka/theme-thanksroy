95b0c40b96e3058885652cd0fc062ef6db7dc1d2
Kim Nguyen <design@kimisgold.com>
2021-06-23 15:41:14 -0400

    Remove extra paragraph tags. Fix #48

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
 <div id="primary">
     <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
-    <p><?php echo $homepageText; ?></p>
+    <?php echo $homepageText; ?>
     <?php endif; ?>
     <?php if (get_theme_option('Display Featured Item') == 1): ?>
     <!-- Featured Item -->
